0582f1cc5fc0695a3a9f4c00045fbd35e698bb22
crazyman <me@crazyman.com>
2023-11-27 17:57:06 +0800

    add gin middleware

---
+++ middleware.go
@@ -0,0 +1,33 @@
+package ucgo
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// UserAuthGinMiddleware gin 用户过滤中间件
+func UserAuthGinMiddleware(decoder JwtEncoder) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		forbidden := func() {
+			ctx.JSON(http.StatusForbidden, gin.H{"status": "Access Denied"})
+		}
+		token := ctx.GetHeader("Authorization")
+		if token == "" {
+			token, _ = ctx.GetQuery("token")
+		}
+		if token == "" {
+			token, _ = ctx.GetPostForm("token")
+		}
+		if token == "" {
+			forbidden()
+			return
+		}
+		user, err := decoder.decodeJwt(token)
+		if err != nil {
+			forbidden()
+			return
+		}
+		ctx.Set("userId", user.Id)
+		ctx.Set("user", user)
+	}
+}
